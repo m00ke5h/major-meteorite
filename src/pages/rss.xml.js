4debfdd8feb815acae56013c6f9ef51932a84948
m00ke5h <pkdash1947.er@gmail.com>
2023-03-22 19:27:02 +0530

    Update README.md

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss'
 
 export async function get() {
     return rss({
-        title: 'Astro Learner | Blog',
+        title: 'Major Meteorite | Blog',
         description: 'My journey learning Astro',
         site: 'https://my-blog-site.netlify.app',
         items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
